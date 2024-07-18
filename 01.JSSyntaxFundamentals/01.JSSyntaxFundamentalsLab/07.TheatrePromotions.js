6a5fe771facbf85bf9fe683f08c3e87b0aaf5105
Tsvetoslav Tsekov <129774811+tsekovTriesCoding@users.noreply.github.com>
2024-07-18 22:07:32 +0300

    add 07.TheatrePromotions

---
+++ 01.JSSyntaxFundamentals/01.JSSyntaxFundamentalsLab/07.TheatrePromotions.js
@@ -0,0 +1,50 @@
+function solve(day, age) {
+
+    let result;
+
+    if (age >= 0 && age <= 18) {
+        switch (day) {
+            case "Weekday":
+                result = "12$";
+                break;
+            case "Weekend":
+                result = "15$";
+                break;
+            case "Holiday":
+                result = "5$";
+                break;
+        }
+    } else if (age > 18 && age <= 64) {
+        switch (day) {
+            case "Weekday":
+                result = "18$";
+                break;
+            case "Weekend":
+                result = "20$";
+                break;
+            case "Holiday":
+                result = "12$";
+                break;
+        }
+    } else if (age > 64 && age <= 122) {
+        switch (day) {
+            case "Weekday":
+                result = "12$";
+                break;
+            case "Weekend":
+                result = "15$";
+                break;
+            case "Holiday":
+                result = "10$";
+                break;
+        }
+    } else {
+        result = "Error!";
+    }
+
+    console.log(result);
+}
+
+solve('Weekday', 42);
+solve('Holiday', -12);
+solve('Holiday', 15);
