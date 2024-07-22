b02dfe9efaa285c3a95ce5205af6e5bdd8ca1da3
Tsvetoslav Tsekov <129774811+tsekovTriesCoding@users.noreply.github.com>
2024-07-22 21:15:27 +0300

    add 04.Substring

---
+++ 02.JSArrays&Strings/01.JSArrays&StringsLab/04.Substring.js
@@ -0,0 +1,8 @@
+function solve(string, startIndex, count) {
+    let substring = string.substring(startIndex, startIndex + count);
+
+    console.log(substring);
+}
+
+solve('ASentence', 1, 8);
+solve('SkipWord', 4, 7);
