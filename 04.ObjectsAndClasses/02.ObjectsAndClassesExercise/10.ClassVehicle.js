bc0add5765e414be8cf5360c17c3f23f3428a45b
Tsvetoslav Tsekov <129774811+tsekovTriesCoding@users.noreply.github.com>
2024-08-08 22:17:57 +0300

    add 10.ClassVehicle

---
+++ 04.ObjectsAndClasses/02.ObjectsAndClassesExercise/10.ClassVehicle.js
@@ -0,0 +1,52 @@
+function solve() {
+
+    class Vehicle {
+        #parts = {};
+
+        constructor(type, model, parts, fuel) {
+            this.type = type;
+            this.model = model;
+            this.parts = parts;
+            this.fuel = fuel;
+        }
+
+        set parts(value) {
+            this.#parts = {
+                engine: value.engine,
+                power: value.power,
+                quality: value.engine * value.power,
+            }
+        }
+
+        get parts() {
+            return this.#parts;
+        }
+
+        getPartsQuality() {
+            return this.parts.engine * this.parts.power;
+        }
+
+        drive(fuelLoss) {
+            this.fuel -= fuelLoss;
+        }
+    }
+    // let parts = { engine: 9, power: 500 };
+
+    // let vehicle = new Vehicle('l', 'k', parts, 840);
+
+    // vehicle.drive(20);
+
+    // console.log(vehicle.fuel);
+
+    let parts = { engine: 6, power: 100 };
+
+    let vehicle = new Vehicle('a', 'b', parts, 200);
+
+    vehicle.drive(100);
+
+    console.log(vehicle.fuel);
+
+    console.log(vehicle.parts.quality);
+}
+
+solve();
