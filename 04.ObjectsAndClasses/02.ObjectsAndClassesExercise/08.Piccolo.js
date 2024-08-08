51925a6e5e2efb9acd2602c14c4e428a37370fba
Tsvetoslav Tsekov <129774811+tsekovTriesCoding@users.noreply.github.com>
2024-08-08 22:17:35 +0300

    add 08.Piccolo

---
+++ 04.ObjectsAndClasses/02.ObjectsAndClassesExercise/08.Piccolo.js
@@ -0,0 +1,24 @@
+function solve(input) {
+    let parking = new Set();
+
+    for (const line of input) {
+        const [direction, carNumber] = line.split(", ");
+
+        direction === "IN" ? parking.add(carNumber) : parking.delete(carNumber);
+    }
+
+    if (parking.size == 0) {
+        console.log("Parking Lot is Empty");
+    } else {
+        Array.from(parking.values())
+        .sort((a, b) => a.localeCompare(b))
+        .forEach(car => console.log(car));
+    }
+
+}
+
+solve(['IN, CA2844AA', 'IN, CA1234TA', 'OUT, CA2844AA', 'IN, CA9999TT', 'IN, CA2866HI', 'OUT, CA1234TA', 'IN, CA2844AA', 'OUT, CA2866HI', 'IN, CA9876HH', 'IN, CA2822UU']);
+solve(['IN, CA2844AA',
+    'IN, CA1234TA',
+    'OUT, CA2844AA',
+    'OUT, CA1234TA']);
